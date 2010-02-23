02a2be43e3880905f4a6e771dab35abf0a1aa487
Kang-min Liu <gugod@gugod.org>
2010-02-23 23:29:00 +0800

    the project need to have trackers in order to start tracking issues.

--- t/net_redmine_test.pl
+++ t/net_redmine_test.pl
@@ -10,32 +10,37 @@ my $REDMINE_RAILS_ROOT = $ENV{NET_REDMINE_RAILS_ROOT};
 my $REDMINE_SERVER_PID = undef;
 
 END {
-    system "kill -9 $REDMINE_SERVER_PID" if $REDMINE_SERVER_PID
+    # system "kill -9 $REDMINE_SERVER_PID" if $REDMINE_SERVER_PID
 }
 
 sub net_redmine_test {
-    return ("http://localhost:3000", "admin", "admin");
+    return ("http://localhost:3000/projects/test", "admin", "admin");
 }
 
 sub new_net_redmine {
-    system "kill -9 $REDMINE_SERVER_PID" if $REDMINE_SERVER_PID;
-    $REDMINE_SERVER_PID = undef;
+    if ($REDMINE_SERVER_PID) {
+        system "kill -9 $REDMINE_SERVER_PID";
+        unlink "tmp/pids/server.pid";
+        $REDMINE_SERVER_PID = undef;
+    }
 
     {
         my $cwd = getcwd;
+        print STDERR "# Starting Redmine Server\n";
 
         chdir $REDMINE_RAILS_ROOT;
-        system q[echo en | rake log:clear db:drop db:create db:migrate redmine:load_default_data];
-        system q[script/runner 'p = Project.create(:name => "test", :identifier => "test", :is_public => false); p.enabled_module_names = ["issue_tracking"]; p.set_parent!(nil); p.save'];
-        system q[script/server -d];
-        sleep 3;
+        unlink "tmp/pids/server.pid";
+        system q[echo en | rake db:drop db:create db:migrate redmine:load_default_data  >/dev/null];
+        system q[script/runner 'p = Project.create(:name => "test", :identifier => "test", :is_public => false); p.enabled_module_names = ["issue_tracking"]; p.trackers = Tracker.all; p.set_parent!(nil); p.save'  >/dev/null];
+        system q[script/server -d >/dev/null];
+        sleep 10; # It is THAT slow on my machine...
 
         $REDMINE_SERVER_PID = `cat tmp/pids/server.pid`;
-        print STDERR "Redmine Server started. PID ${REDMINE_SERVER_PID}\n";
+        print STDERR "# Redmine Server started. PID ${REDMINE_SERVER_PID}\n";
         chdir $cwd;
     }
 
-    my ($server, $user, $password) = net_redmine_test();
+    my ($server, $user, $password) = ("http://localhost:3000/projects/test", "admin", "admin");
     return Net::Redmine->new(url => $server,user => $user, password => $password);
 }
 
